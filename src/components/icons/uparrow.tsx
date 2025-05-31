f4afbb9ffaf2c5446f9fa588444d35baf0f6ce85
Alex Wilson <apreswilson@gmail.com>
2025-05-31 13:03:30 -0400

    Deploy file updates

--- src/components/icons/uparrow.tsx
+++ src/components/icons/uparrow.tsx
@@ -1,23 +1,13 @@
-import React from "react";
-
 type IconProps = {
-  className: string;
+	className: string;
 };
 
 export default function UpArrowIcon(props: Readonly<IconProps>) {
-  return (
-    <svg
-      className={props.className}
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g strokeLinecap="butt" strokeLinejoin="miter">
-        <polyline
-          points="22,17 12,7 2,17"
-          strokeLinecap="square"
-          strokeMiterlimit="10"
-        />
-      </g>
-    </svg>
-  );
+	return (
+		<svg className={props.className} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+			<g strokeLinecap="butt" strokeLinejoin="miter">
+				<polyline points="22,17 12,7 2,17" strokeLinecap="square" strokeMiterlimit="10" />
+			</g>
+		</svg>
+	);
 }
